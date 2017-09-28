3a261c54955d9af787720374e8f838805d1dd371
dduenow <davidduenow@mwc-123017.dhcp.missouri.edu>
2017-09-28 13:38:55 -0500

    Awesome commit message here!

--- GitWorkflow/ViewController.swift
+++ GitWorkflow/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //Awesome Programming
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
